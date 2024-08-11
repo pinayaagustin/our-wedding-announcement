60e8c4ca23743046c66d712b04b41de002b61e79
pinaya agustin <agustinpinaya09@gmail.com>
2024-08-11 18:07:53 +0700

    cek yang

--- app/page.js
+++ app/page.js
@@ -1,6 +1,20 @@
+import './globals.css';
 
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <div className="container mx-auto px-4 py-8 text-center">
+      <div className="mb-10">
+        <h1 className="text-5xl font-bold text-amber-700">Kukuh Iman Damaryanto</h1>
+        <h1 className="text-5xl font-bold text-amber-700">&</h1>
+        <h1 className="text-5xl font-bold text-amber-700">Pinaya Agustin</h1>
+      </div>
+
+      <p className="text-xl text-gray-700 mb-4">
+        Dengan penuh cinta dan sukacita, kami ingin memberikan kabar bahagia yaitu akan dilangsungkannya pernikahan kami pada:
+      </p>
+      <p className="text-2xl font-semibold text-pink-500 mb-8">
+        Jum'at, 6 Desember 2024
+      </p>
+    </div>
   )
 }
