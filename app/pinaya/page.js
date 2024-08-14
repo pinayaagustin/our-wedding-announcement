6f2a41e495b83e684222aa57cf3213bb5fa43590
pinaya agustin <agustinpinaya09@gmail.com>
2024-08-14 20:32:10 +0700

    update banyak bangettt

--- app/pinaya/page.js
+++ app/pinaya/page.js
@@ -1,19 +1,32 @@
+'use client'
 import { Great_Vibes } from 'next/font/google';
+import { useState } from 'react';
 import '../globals.css';
 
 const great = Great_Vibes({weight:['400'],subsets:['latin']})
 
 export default function Home() {
+    const [messages, setMessages] = useState([]);
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (name && message) {
+            setMessages([...messages, { name, message }]);
+            setName('');
+            setMessage('');
+        }
+    };
+
   return (
     <div className="container mx-auto px-6 py-10 flex flex-col gap-10">
-        
         <div className='flex flex-col gap-10 mt-12'>
             <p className="sm:text-lg text-sm text-gray-700 text-justify">
                 Dengan penuh cinta dan sukacita, kami bermaksud membagikan kabar bahagia ini sekaligus memohon doa dan restu
                 dari teman-teman sekalian untuk pernikahan kami :
             </p>
         </div>
-        
         <div className="flex flex-row justify-between items-center">
             <div className="w-1/2 p-4">
             <p className={"sm:text-7xl text-2xl font-extrabold text-blue-900 text-left " + great.className}>
@@ -39,31 +52,57 @@ export default function Home() {
             </div>
         </div>
 
-            <p className="text-sm text-gray-700 text-center" >yang akan dilaksanakan pada :</p>
+            <p className="text-lg text-gray-700 text-center mt-10" >yang akan dilaksanakan pada :</p>
             <p className="text-2xl font-semibold text-blue-900 text-center ">Jum'at, 6 Desember 2024</p>
         
-        
+        <div>
+            <p>
+                Bingung ya? "Kok ga ada informasi lokasi dan waktu?"
+            </p>
+            <p>
+                Yups! Kami memutuskan untuk mengadakan acara pernikahan kami secara sederhana, 
+                yang hanya akan dihadiri oleh keluarga besar dan kerabat dekat kami saja. Meski 
+                begitu, kehadiran kalian di hati kami serta doa dan dukungan dari kalian akan selalu
+                berarti dan menjadi anugerah yang tak ternilai.
+            </p>
+        </div>
         {/* <div className="flex items-center justify-center"> */}
             <div className="flex sm:flex-row flex-col w-full gap-8 mt-20">
-                <form className="space-y-4 sm:w-1/2 w-full p-4 bg-white shadow-md rounded-2xl">
+                <form onSubmit={handleSubmit} className="space-y-4 sm:w-1/2 w-full p-4 bg-orange-100 shadow-md rounded-2xl">
                 <h3 className="text-center font-semibold text-gray-700">Tulis Ucapan Disini</h3>
                 <div>
                     <label className="block text-left text-gray-700 text-sm font-medium mb-2">Nama :</label>
-                    <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-500" placeholder="Nama Anda" required/>
+                    <input 
+                        type="text"
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-500" placeholder="Nama Anda" required/>
                 </div>
 
                 <div>
                     <label className="block text-left text-gray-700 text-sm font-medium mb-2">Ucapan Selamat :</label>
-                    <textarea className="w-full px-4 py-4 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-500" placeholder="Tulis ucapan selamat Anda disini ..." required></textarea>
+                    <textarea 
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        className="w-full px-4 py-4 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-500" placeholder="Tulis ucapan selamat Anda disini ..." required>   
+                    </textarea>
                 </div>
 
                 <div className="flex justify-center">
-                <button type="submit" className="text-center bg-slate-200 text-gray-700 py-2 px-4 rounded-2xl hover:bg-slate-400 transition duration-300">Kirim</button>
+                <button type="submit" className="text-center bg-slate-200 text-gray-700 py-2 px-4 rounded-2xl hover:bg-orange-200 transition duration-300">Kirim</button>
                 </div>
                 </form>
 
-                <div className="mx-auto flex-auto sm:w-1/2 w-full p-4 bg-white shadow-md rounded-2xl ">
-                    <h3 className="text-center font-semibold text-gray-700 mb-4">Ucapan Selamat yang Diterima</h3>
+                <div className="mx-auto flex-auto sm:w-1/2 w-full p-4 bg-orange-100 shadow-md rounded-2xl overflow-y-auto max-h-96">
+                    <h3 className="text-center font-semibold text-gray-700 mb-4">Doa-doa dari kalian sangat berarti bagi kami, terima kasih!</h3>
+                    <div className="space-y-4">
+                        {messages.map((msg, index) => (
+                            <div key={index} className="p-4 bg-white rounded-md shadow-sm">
+                                <p className="font-bold text-gray-700">{msg.name}</p>
+                                <p className="text-ray-600">{msg.message}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         {/* </div> */}
