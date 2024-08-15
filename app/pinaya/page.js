b55ffe78264e79cdc0dd23f025454166124df439
pinaya agustin <agustinpinaya09@gmail.com>
2024-08-15 21:03:46 +0700

    ..

--- app/pinaya/page.js
+++ app/pinaya/page.js
@@ -8,7 +8,7 @@ import '../globals.css';
 const great = Great_Vibes({weight:['400'],subsets:['latin']})
 
 export default function Home() {
-    const [isPlaying, setIsPlaying] = useState(false);
+    const [isPlaying, setIsPlaying] = useState(true);
     const audioRef = useRef(null);
 
     useEffect(() => {
@@ -39,7 +39,7 @@ export default function Home() {
   return (
     <div className="container mx-auto px-6 py-10 flex flex-col gap-10">
         <audio ref={audioRef} src="/music.mp3" loop className="hidden"></audio>
-        <button onClick={toggleAudio} className="absolute top-4 right-4 bg-blue-300 hover:bg-blue-900 text-white p-3 rounded-full">
+        <button onClick={toggleAudio} className="fixed top-4 right-4 z-50 bg-blue-300 hover:bg-blue-900 text-white p-3 rounded-full">
           <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} size="sm" />
         </button>
       
@@ -80,10 +80,7 @@ export default function Home() {
         
         <div>
             <p>
-                Bingung ya? "Kok ga ada informasi lokasi dan waktu?"
-            </p>
-            <p>
-                Yups! Kami memutuskan untuk mengadakan acara pernikahan kami secara sederhana, 
+                Kami memutuskan untuk mengadakan acara pernikahan kami secara sederhana, 
                 yang hanya akan dihadiri oleh keluarga besar dan kerabat dekat kami saja. Meski 
                 begitu, kehadiran kalian di hati kami serta doa dan dukungan dari kalian akan selalu
                 berarti dan menjadi anugerah yang tak ternilai.
@@ -91,7 +88,7 @@ export default function Home() {
         </div>
         {/* <div className="flex items-center justify-center"> */}
             <div className="flex sm:flex-row flex-col w-full gap-8 mt-20">
-                <form onSubmit={handleSubmit} className="space-y-4 sm:w-1/2 w-full p-4 bg-white shadow-md rounded-2xl">
+                <form onSubmit={handleSubmit} className="space-y-4 sm:w-1/2 w-full p-4 bg-orange-100 shadow-md rounded-2xl">
                 <h3 className="text-center font-semibold text-gray-700">Tulis Ucapan Disini</h3>
                 <div>
                     <label className="block text-left text-gray-700 text-sm font-medium mb-2">Nama :</label>
@@ -116,7 +113,7 @@ export default function Home() {
                 </div>
                 </form>
 
-                <div className="mx-auto flex-auto sm:w-1/2 w-full p-4 bg-white shadow-md rounded-2xl overflow-y-auto max-h-96">
+                <div className="mx-auto flex-auto sm:w-1/2 w-full p-4 bg-orange-100 shadow-md rounded-2xl overflow-y-auto max-h-96">
                     <h3 className="text-center font-semibold text-gray-700 mb-4">Doa-doa dari kalian sangat berarti bagi kami, terima kasih!</h3>
                     <div className="space-y-4">
                         {messages.map((msg, index) => (
