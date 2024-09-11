8aaa90f397413e5b1f9b09530e5a09ad5168ce38
ofcucuh <ofckukuh@gmail.com>
2024-09-11 20:55:04 +0700

    add animation

--- app/envel/page.js
+++ app/envel/page.js
@@ -1,54 +1,67 @@
 'use client'
-// components/Envelope.js
-import { delay, motion } from 'framer-motion';
-// import './Envelope.css'; // Menggunakan CSS module untuk styling
-import { useState } from 'react';
-
+import { motion } from 'framer-motion';
+import { useState, useEffect } from 'react';
+import { Great_Vibes } from 'next/font/google';
+const great = Great_Vibes({weight:['400'],subsets:['latin']})
 const Envelope = () => {
-  // Variasi animasi untuk bagian amplop
   const envelopeVariants = {
     closed: { transform: 'matrix(1, 0, 0, -1, 489.99951171875, 192)'},
     open: { transform: 'matrix(1, 0, 0, 1, 489.99951171875, -1)' },
-    mailGoDown:{y:0},
-    mailGoUp:{y:40},
-    goUp:{y:0, height:400, transition: {duration:0.5, ease: 'easeOut'}},
-    goDown:{y:400, height:0, transition: {duration:1}}
+    goUp:{y:-70, height:500, transition: {duration:1, delay:1.5, ease: 'easeOut'}},
+    goDown:{y:500, height:0, transition: {duration:1, ease: 'easeOut'}},
+    zoomIn: { scale: 4, y:90, transition: { duration: 3, ease: 'easeInOut' } },
+    zoomOut:{ scale: 1}
   };
 
   const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <div className='flex justify-center items-center w-fit' onClick={() => setIsOpen(true)}>
-        <motion.svg viewBox="4.998 -70 480 500" width="100%">
-            <motion.rect width="480" height="300" className='fill-[#03346E]' y="95" x="5" />
-            <motion.path d="M -245 -54 L -5 96 L -485 96 L -245 -54 Z" className='fill-[#03346E]' initial= "closed" variants={envelopeVariants} animate={isOpen ? 'open' : 'closed'} transition={{ duration: 0.4 }} />
-            <motion.rect x="6" width="480" className="fill-accent-1" variants={envelopeVariants} initial="goDown" animate={isOpen ? 'goUp' : 'goDown'} />
-            <motion.path d="M 5.002 95 L 485.002 395 L 5.002 395 L 5.002 95 Z" className="fill-[#1C5AA5]" />
-            <motion.path d="M 485.002 95 L 485.002 395 L 5.002 395 L 485.002 95 Z" className='fill-[#055A9B]' />
+    <motion.div className='flex justify-center items-center w-fit' onClick={() => setIsOpen(true)} variants={envelopeVariants} initial="zoomOut" animate={isOpen ? 'zoomIn' : 'zoomOut'} transition={{ duration: 3 }}>
+        <motion.svg viewBox="5 -70 480 500" width="80%">
+            <motion.rect width="480" height="300" className='fill-[#9b3b48]' y="95" x="5" /> 
+            <motion.path d="M -245 -54 L -5 96 L -485 96 L -245 -54 Z" className='fill-[#9b3b48]' initial= "closed" variants={envelopeVariants} animate={isOpen ? 'open' : 'closed'} transition={{ duration: 1,delay:0.5 }} />
+            <motion.rect x="5" width="480" className="fill-primary" variants={envelopeVariants} initial="goDown" animate={isOpen ? 'goUp' : 'goDown'} />
+            <motion.path d="M 5 95 L 485 395 L 5 395 L 5 95 Z" className="fill-[#b77c84]" />
+            <motion.path d="M 485 95 L 485 395 L 5 395 L 485 95 Z" className='fill-[#e58a96]' />
         </motion.svg>
-        {/* <?xml version="1.0" encoding="utf-8"?>
-        <svg viewBox="4.999 96 480 150" width="480" height="150" xmlns="http://www.w3.org/2000/svg" xmlns:bx="https://boxy-svg.com">
-        <path d="M -245 -246 L -5 -96 L -485 -96 L -245 -246 Z" style="stroke: rgb(0, 0, 0); stroke-width: 0px; fill: rgb(3, 52, 110); transform-origin: -245px -96px;" transform="matrix(-1, 0, 0, -1, 489.9990234375, 192.000045776367)" bx:shape="triangle -485 -246 480 150 0.5 0 1@851d3022"/>
-        </svg> */}
-        {/* <svg viewBox="4.998 -54 480 449" width="480" height="449" xmlns="http://www.w3.org/2000/svg" xmlns:bx="https://boxy-svg.com"> */}
-            {/* <path />
-            <?xml version="1.0" encoding="utf-8"?>
-<svg viewBox="36.165 180.361 417.669 194" width="417.669" height="194" xmlns="http://www.w3.org/2000/svg">
-  <rect x="36.165" y="180.361" width="417.669" height="194" style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0); stroke-opacity: 0; stroke-width: 0px; transform-origin: 244.999px 180.4px;"/>
-</svg>
-            <rect width="480" height="300" style="stroke: rgb(0, 0, 0); stroke-width: 0px; fill: rgb(3, 52, 110);" y="94.996" x="4.998"/>
-            <path d="M 5.002 95 L 485.002 395 L 5.002 395 L 5.002 95 Z" style="stroke: rgb(0, 0, 0); stroke-width: 0px; fill: rgb(28, 90, 165);" bx:shape="triangle 5.002 95 480 300 0 0 1@02da12c2"/>
-            <path d="M 485.002 95 L 485.002 395 L 5.002 395 L 485.002 95 Z" style="stroke-width: 0px; paint-order: fill; stroke: rgb(0, 0, 0); fill: rgb(5, 90, 155);" bx:shape="triangle 5.002 95 480 300 1 0 1@85b689fd"/> */}
-        {/* </svg> */}
-    </div>
+    </motion.div>
   );
 };
 
-export default function Home() {
+export default function Envel({setOpenParent}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [windowHeight, setWindowHeight] = useState(0);
+
+  const envelopeVariants = {
+    slideDown:{y:windowHeight},
+    slideStandby:{y:0}
+  }
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // console.log(window.innerHeight,window.screenY)
+      setWindowHeight(window.innerHeight);
+    }
+  }, []);
     return (
-      <div className='flex items-center justify-center h-svh gap-10'>
-        <h1>edas </h1>
-        <Envelope />
-      </div>
+      <motion.div className='overflow-hidden bg-primary'>
+        <motion.div className='flex items-center justify-center flex-col h-svh gap-10 bg-white overflow-hidden' animate={isOpen ? 'slideDown' : 'slideStandby'} variants={envelopeVariants} initial="slideStandby" transition={{duration:1,delay:4,ease:'easeInOut'}}>
+          <p className={great.className + ' sm:text-5xl text-3xl text-center w-[80%] text-primary' } > Hari Bahagia Kami<br/>Segera Tiba! </p>
+          <motion.div animate={isOpen ? {scale: 1} : {
+            scale: [1, 1.05, 1],
+            transition: {
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: 'loop',
+              ease: 'easeInOut'
+            }
+          }} onClick={()=>{
+              setIsOpen(true)
+              setOpenParent(true)
+            }}>
+            <Envelope />
+          </motion.div>
+          <p className=' sm:text-5xl text-sm text-center w-[80%] text-primary' > Ketuk pada gambar untuk melanjutkan </p>
+        </motion.div>
+      </motion.div>
+      
     );
   }
