7841809b0ba8f4f5fdd193bcf2749fc32886287f
ofcucuh <ofckukuh@gmail.com>
2024-11-18 14:30:38 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Playfair_Display } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Playfair_Display({ weight: ['400','500','600','700','800','900'], subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className}`}>
       {children}
+      <SpeedInsights/>
       <Analytics />
       </body>
     </html>
